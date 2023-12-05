07ec26aa83f0a65ab6b63cdfc9fc6b34acd72216
AncestralBoy <100536878+AncestralBoy@users.noreply.github.com>
2023-12-05 19:58:27 +0100

    Modify the name of Player in Application class

--- src/Application.java
+++ src/Application.java
@@ -4,7 +4,7 @@ import map.domain.Room;
 
 public class Application {
     public static void main(String[] args) {
-        Player player = new Player("Price");
+        Player player = new Player("Prince");
         Room entry = new Room(1, "Basement", "You’re in a dark, cramped place", 0, 2, 3, 6);
         GameController gameController = new GameController(entry, player);
         gameController.runGame();
