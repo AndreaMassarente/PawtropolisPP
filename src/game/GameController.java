e9c0e33c51d4517ea83a8a837aa2aebc03bf2420
AncestralBoy <100536878+AncestralBoy@users.noreply.github.com>
2023-12-05 19:56:46 +0100

    Move the get, drop, look, bag and go methods from the GameController class to Player class

--- src/game/GameController.java
+++ src/game/GameController.java
@@ -9,101 +9,54 @@ import map.domain.Room;
 public class GameController {
     private final Room entry;
     private final Player player;
+    private final MapController mapController;
 
     public GameController(Room entry, Player player) {
         this.entry = entry;
         this.player = player;
+        mapController = new MapController();
     }
 
     public void runGame() {
-        MapController mapController = new MapController();
         Room currentRoom = entry;
+        System.out.println(mapController.getMap().get(currentRoom.getE()));
         boolean gameEnded = false;
 
         while(!gameEnded) {
             String input;
-            System.out.println("Where are you going to go?");
+            System.out.println("What do you want to do? (look, go, bag, get, drop)");
             System.out.print(">");
             input = InputController.readString();
             switch(input){
                 case "look":
-                    System.out.println(currentRoom.getRoomDescription());
+                    System.out.println(lookInRoom(currentRoom));
                     break;
                 case "bag":
-                    System.out.println(player.getMyBag().getItemsInBag());
+                    System.out.println(lookInBag());
                     break;
                 case "go":
-                    System.out.println("Choose direction: n, s, e, w");
-                    System.out.print(">");
-                    input = InputController.readString();
-                    switch (input){
-                        case "n":
-                            if(currentRoom.getN() != 0){
-                                currentRoom = mapController.getMap().get(currentRoom.getN());
-                            }
-                            else{
-                                System.out.println("There's no room here ");
-                            }
-                            break;
-                        case "s":
-                            if(currentRoom.getS() != 0){
-                                currentRoom = mapController.getMap().get(currentRoom.getS());
-                            }
-                            else{
-                                System.out.println("There's no room here ");
-                            }
-                            break;
-                        case "w":
-                            if(currentRoom.getW() != 0){
-                                currentRoom = mapController.getMap().get(currentRoom.getW());
-                            }
-                            else{
-                                System.out.println("There's no room here ");
-                            }
-                            break;
-                        case "e":
-                            if(currentRoom.getE() != 0){
-                                currentRoom = mapController.getMap().get(currentRoom.getE());
-                            }
-                            else{
-                                System.out.println("There's no room here ");
-                            }
-                            break;
-                    }
+                    currentRoom = player.go(mapController, currentRoom);
                     break;
                 case "get":
-                    System.out.println("Choose item to take:\n" + currentRoom.getItemInRoom());
-                    System.out.print(">");
-                    String inputItemToTake = InputController.readString();
-                    Item chosenItemToTake = currentRoom.getItems()
-                            .stream()
-                            .filter(i -> i.getName().equals(inputItemToTake))
-                            .findFirst()
-                            .orElseGet(() -> null);
-                    if(chosenItemToTake != null){
-                        player.getMyBag().addItemInBag(chosenItemToTake);
-                        currentRoom.removeItem(chosenItemToTake);
-                    }
+                    currentRoom.removeItem(player.get(currentRoom));
                     break;
                 case "drop":
-                    System.out.println("Choose item to drop:\n" + player.getMyBag().getItemsInBag());
-                    System.out.print(">");
-                    String inputItemToDrop = InputController.readString();
-                    Item chosenItemToDrop = player.getMyBag()
-                            .getItems()
-                            .stream()
-                            .filter(i -> i.getName().equals(inputItemToDrop))
-                            .findFirst()
-                            .orElseGet(() -> null);
-                    if(chosenItemToDrop != null){
-                        currentRoom.addItem(chosenItemToDrop);
-                        player.getMyBag().removeItemFromBag(chosenItemToDrop);
-                    }
+                    currentRoom.addItem(player.drop(currentRoom));
                     break;
+                default:
+                    System.out.println("Invalid command.");
             }
             if (input.equals("exit")) {
                 gameEnded = true;
             }
         }
+    }
+
+    private String lookInRoom(Room room){
+        return room.getRoomDescription();
+    }
+
+    private String lookInBag(){
+        return player.getMyBag().getItemsInBag();
     }
 }
