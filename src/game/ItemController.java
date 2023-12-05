71051e79cc1ee058133d586eb133ac3bbd4bb6cd
AncestralBoy <100536878+AncestralBoy@users.noreply.github.com>
2023-12-05 09:56:18 +0100

    Add generateItems method in ItemController class

--- src/game/ItemController.java
+++ src/game/ItemController.java
@@ -18,4 +18,17 @@ public class ItemController {
     public List<Item> getItems() {
         return items;
     }
+
+    public void generateItems(){
+        items.add(new Item("Potion", "Heals 20 HP", 1));
+        items.add(new Item("Super Potion", "Heals 50 HP", 1));
+        items.add(new Item("Hyper Potion", "Heals 200 HP", 2));
+        items.add(new Item("Full Heal", "Heals all status conditions", 1));
+        items.add(new Item("Full Restore", "Heals all HP and status conditions", 2));
+        items.add(new Item("Sword", "+10 atk", 1));
+        items.add(new Item("Shield", "+10 def", 1));
+        items.add(new Item("Armor", "+50 def, -20 speed", 2));
+        items.add(new Item("Bow", "+10 sp.atk, +10speed", 1));
+        items.add(new Item("Gun", "+20 sp.atk, +20 sp.def", 2));
+    }
 }
