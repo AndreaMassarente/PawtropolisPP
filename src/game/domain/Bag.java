c076631c75930b5cd5293a5d92f9102b6fc2da6a
AncestralBoy <100536878+AncestralBoy@users.noreply.github.com>
2023-12-05 10:35:39 +0100

    Structure development Bag class

--- src/game/domain/Bag.java
+++ src/game/domain/Bag.java
@@ -1,4 +1,36 @@
 package game.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bag {
+    private final int maxSlots;
+    private int currentFreeSlots;
+    private List<Item> items;
+
+    public Bag() {
+        this.maxSlots = 6;
+        this.currentFreeSlots = maxSlots;
+        this.items = new ArrayList<>();
+    }
+
+    public int getMaxSlots() {
+        return maxSlots;
+    }
+
+    public int getCurrentFreeSlots() {
+        return currentFreeSlots;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setCurrentFreeSlots(int currentFreeSlots) {
+        this.currentFreeSlots = currentFreeSlots;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
 }
