46dae667cdb340f2ae5540f57de53a700d1ce901
AncestralBoy <100536878+AncestralBoy@users.noreply.github.com>
2023-12-05 12:51:12 +0100

    Implement command look, bag, go and get, in GameController class

--- src/game/GameController.java
+++ src/game/GameController.java
@@ -1,7 +1,9 @@
 package game;
 
 import game.console.InputController;
+import game.domain.Item;
 import game.domain.Player;
+import map.MapController;
 import map.domain.Room;
 
 public class GameController {
@@ -14,6 +16,8 @@ public class GameController {
     }
 
     public void runGame() {
+        MapController mapController = new MapController();
+        mapController.printKey();
         Room currentRoom = entry;
         boolean gameEnded = false;
 
@@ -22,7 +26,70 @@ public class GameController {
             System.out.println("Where are you going to go?");
             System.out.print(">");
             input = InputController.readString();
+            switch(input){
+                case "look":
+                    System.out.println("You are in Room " + currentRoom.getId() +  "\n" + currentRoom.getDescription() + "\nItems: " + currentRoom.getItems()
+                            +"\nNCP: " + currentRoom.getNpc());
+                    break;
+                case "bag":
+                    System.out.println(player.getMyBag().getItems());
+                    break;
+                case "go":
+                    System.out.println("Choose direction: n, s, e, w");
+                    System.out.print(">");
+                    input = InputController.readString();
+                    switch (input){
+                        case "n":
+                            if(currentRoom.getN() != 0){
+                                currentRoom = mapController.getMap().get(currentRoom.getN());
+                            }
+                            else{
+                                System.out.println("There's no room here ");
+                            }
+                            break;
+                        case "s":
+                            if(currentRoom.getS() != 0){
+                                currentRoom = mapController.getMap().get(currentRoom.getS());
+                            }
+                            else{
+                                System.out.println("There's no room here ");
+                            }
+                            break;
+                        case "w":
+                            if(currentRoom.getW() != 0){
+                                currentRoom = mapController.getMap().get(currentRoom.getW());
+                            }
+                            else{
+                                System.out.println("There's no room here ");
+                            }
+                            break;
+                        case "e":
+                            if(currentRoom.getE() != 0){
+                                currentRoom = mapController.getMap().get(currentRoom.getE());
+                            }
+                            else{
+                                System.out.println("There's no room here ");
+                            }
+                            break;
+                    }
+                    break;
+                case "get":
+                    System.out.println("Choose item to take:\n" + currentRoom.getItems());
+                    System.out.print(">");
+                    Item chosenItem = currentRoom.getItems()
+                            .stream()
+                            .filter(i -> i.getName().equals(InputController.readString()))
+                            .findFirst()
+                            .orElseGet(() -> null);
+                    if(chosenItem != null){
+                        //Metodo in Room per rimozione item
+                    }
+                    break;
+                case "drop":
 
+                    break;
+
+            }
             /*
              * Inserire qui la gestione degli altri comandi
              */
