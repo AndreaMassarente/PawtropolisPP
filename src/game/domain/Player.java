4d05d651c53de55f1c17c070fc726d8c4ae68d9e
AncestralBoy <100536878+AncestralBoy@users.noreply.github.com>
2023-12-05 10:36:15 +0100

    Structure development Player class

--- src/game/domain/Player.java
+++ src/game/domain/Player.java
@@ -1,4 +1,25 @@
 package game.domain;
 
 public class Player {
+    private final String name;
+    private int healthPoints;
+    private Bag myBag;
+
+    public Player(String name, int healthPoints) {
+        this.name = name;
+        this.healthPoints = healthPoints;
+        myBag = new Bag();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealthPoints() {
+        return healthPoints;
+    }
+
+    public void setHealthPoints(int healthPoints) {
+        this.healthPoints = healthPoints;
+    }
 }
