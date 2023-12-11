711d79bc8096b4d73d7a26b927b94eb7f93551ff
Pablo NARDECCHIA <pablo.nardecchia@alten.it>
2023-12-11 10:42:18 +0100

    update Direction class

--- src/map/utils/Direction.java
+++ src/map/utils/Direction.java
@@ -1,8 +1,23 @@
 package map.utils;
 
+import java.util.Arrays;
+
 public enum Direction {
-    NORTH,
-    SOUTH,
-    EAST,
-    WEST;
+    NORTH ("North"),
+    SOUTH ("South"),
+    EAST ("East"),
+    WEST ("West");
+
+    private final String name;
+
+    Direction(String name) {
+        this.name = name;
+    }
+
+    public static Direction getDirection(String s){
+        return Arrays.stream(Direction.values())
+                .filter(d -> d.name.equalsIgnoreCase(s))
+                .findFirst()
+                .orElse(null);
+    }
 }
