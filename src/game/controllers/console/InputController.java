903b6b49c129c387527da8cdd89866ecf522bb65
Pablo NARDECCHIA <pablo.nardecchia@alten.it>
2023-12-12 15:27:26 +0100

    creation of InputController class and insert of reading and splitting words methods

--- src/game/controllers/console/InputController.java
+++ src/game/controllers/console/InputController.java
@@ -1,2 +1,37 @@
-package game.controllers.console;public class InputController {
+package game.controllers.console;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class InputController {
+    public InputController() {
+    }
+
+    public String readCommand(String input){
+        List<String> wordList;
+        String returnString = "";
+        String lowerCaseString = input.trim().toLowerCase();
+
+        if(!lowerCaseString.equals("q")){
+            if (lowerCaseString.isEmpty()){
+                returnString = "You must enter a command";
+            } else {
+                wordList = splitWords(lowerCaseString);
+                //returnString = parseCommand(wordList);
+            }
+        }
+        else
+            return "quit";
+        return returnString;
+    }
+
+    private List<String> splitWords(String input){
+        String delimiters = "[ \t,.:;?!\"']+";
+        String[] words = input.split(delimiters, 2);
+
+        return new ArrayList<>(Arrays.asList(words));
+    }
+
+
 }
