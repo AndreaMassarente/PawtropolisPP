2ccc5f3776d56abf4eaac6565cf3f1081bb9f8c3
Pablo NARDECCHIA <pablo.nardecchia@alten.it>
2024-01-22 10:40:06 +0100

    Move execute method to GameManager class

---
+++ src/main/java/com/example/Pawtropolis/game/service/console/InputHandler.java
@@ -0,0 +1,34 @@
+package com.example.Pawtropolis.game.service.console;
+
+import lombok.extern.java.Log;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+@Log
+public class InputHandler {
+    private InputHandler() {
+    }
+
+    public static List<String> readCommand(String input){
+        List<String> wordList = null;
+        String lowerCaseString = input.trim().toLowerCase();
+
+        if (lowerCaseString.isEmpty()){
+            log.warning("You must enter a command");
+        } else {
+            wordList = splitWords(lowerCaseString);
+        }
+        return wordList;
+    }
+
+    private static List<String> splitWords(String input){
+        String delimiters = "[ \t,.:;?!\"']+";
+        String[] words = input.split(delimiters, 2);
+
+        return new ArrayList<>(Arrays.asList(words));
+    }
+}
